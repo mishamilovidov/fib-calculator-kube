36ad61186dcfb2906d6fd4bfe25829933828a4c9
Misha Milovidov <m.alexeyvich@gmail.com>
2019-07-12 13:46:41 -0700

    👷‍♂️ Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+# build and tag images
+docker build \
+  -t mishamilovidov/multi-client:latest \
+  -t mishamilovidov/multi-client:$SHA \
+  -f ./client/Dockerfile ./client
+
+docker build \
+  -t mishamilovidov/multi-server:latest \
+  -t mishamilovidov/multi-server:$SHA \
+  -f ./server/Dockerfile ./server
+
+docker build \
+  -t mishamilovidov/multi-worker:latest \
+  -t mishamilovidov/multi-worker:$SHA \
+  -f ./worker/Dockerfile ./worker
+
+
+# push built images to registry
+docker push mishamilovidov/multi-client:latest
+docket push mishamilovidov/multi-client:$SHA
+
+docker push mishamilovidov/multi-server:latest
+docket push mishamilovidov/multi-server:$SHA
+
+docker push mishamilovidov/multi-worker:latest
+docket push mishamilovidov/multi-worker:$SHA
+
+# apply configs to k8s cluster
+kubectl apply -f k8s
+
+# set images for k8s deployment objects
+kubectl set image deployments/client-deployment client=mishamilovidov/multi-client:$SHA
+kubectl set image deployments/server-deployment server=mishamilovidov/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=mishamilovidov/multi-worker:$SHA
